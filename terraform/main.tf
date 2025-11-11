eb6734349ee5cfc07ca235132d4a5fcfd4c4fd86
Cleo Howard <lolearningcode@gmail.com>
2025-11-10 18:32:07 -0800

    Fix policy violations: add encryption and owner tag

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,16 @@ resource "aws_s3_bucket" "example" {
 
   tags = {
     environment = var.environment
-    # owner       = var.owner
+    owner       = var.owner
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
   }
 }
